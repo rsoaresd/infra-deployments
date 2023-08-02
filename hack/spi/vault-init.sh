74a9c091538a85fc548ecf33ff11066aa035a7c5
Rafaela Soares <rsoaresd@redhat.com>
2023-08-02 14:11:06 +0100

    c

--- hack/spi/vault-init.sh
+++ hack/spi/vault-init.sh
@@ -34,7 +34,7 @@ function init() {
 	echo "$SECRET" == "not found"
 
 	# if secret does not exist in the second attempt, it means that something went wrong in the first one
-	if [[ "$INIT_STATE" == "false" || "$SECRET" == "not found" ]]; then
+	if [[ "$SECRET" == "not found" ]]; then
 		vaultExec "vault operator init" >"${KEYS_FILE}"
 		echo "Keys written at ${KEYS_FILE}"
 	elif [ "$INIT_STATE" == "true" ]; then
