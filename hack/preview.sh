7cf91f12559796c8ab300f5706dec6bf08e3a852
Rafaela Soares <rsoaresd@redhat.com>
2023-09-07 18:30:01 +0100

    add PREVIEW_BRANCH env

--- hack/preview.sh
+++ hack/preview.sh
@@ -105,6 +105,11 @@ fi
 
 # Create preview branch for preview configuration
 PREVIEW_BRANCH=preview-${MY_GIT_BRANCH}${TEST_BRANCH_ID+-$TEST_BRANCH_ID}
+
+# Save PREVIEW_BRANCH to keep track of the preview branch
+# In case of a e2e-tests ci job, it makes sense to delete this branch after running the tests
+export PREVIEW_BRANCH= $PREVIEW_BRANCH
+
 if git rev-parse --verify $PREVIEW_BRANCH &> /dev/null; then
     git branch -D $PREVIEW_BRANCH
 fi
